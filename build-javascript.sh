1697ead671b976197272dbe3712e480110e74706
Riccardo Balbo <riccardo0blb@gmail.com>
2024-04-13 10:28:03 +0200

    improve package for better ts compatibility

--- build-javascript.sh
+++ build-javascript.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 set -e
-mkdir -p build/javascript/src
-mkdir -p build/javascript/dist
+mkdir -p build/javascript
+
 
 npm i --save-dev @grpc/grpc-js ts-protoc-gen @protobuf-ts/plugin typescript @types/node
 
-npx protoc --ts_out build/javascript/src \
+npx protoc --ts_out build/javascript \
 --experimental_allow_proto3_optional \
 --ts_opt long_type_number,server_generic \
  --proto_path proto proto/*.proto
 
 cp -Rvf npm/* build/javascript/
 cd build/javascript
-npm run build
 node generate_imports.cjs
+
+npm run build
+
 npm pack
--- npm/generate_imports.cjs
+++ npm/generate_imports.cjs
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-const distDir = path.join(__dirname, 'dist');
-const indexFile = path.join(__dirname, 'index.js');
+const distDir = path.join(__dirname, '.');
+const indexFile = path.join(__dirname, 'index.ts');
 
 let ex = '';
 fs.readdirSync(distDir).forEach(file => {
-    if (path.extname(file) === '.js') {
-        ex += `export * from './dist/${file}';\n`;
+    if (path.extname(file) === '.ts') {
+        const base = file.slice(0, -3);
+        ex += `export * from './${base}';\n`;
     }
 });
 console.log("Write index", indexFile);
