4ebe6049d61879ed4d98f63ea4625bbd35d0ba0c
Riccardo Balbo <riccardo0blb@gmail.com>
2024-04-17 11:19:19 +0200

    Add php build

--- build-php.sh
+++ build-php.sh
@@ -2,10 +2,12 @@
 set -e
 mkdir -p tmp
 mkdir -p build/php
-pecl install grpc
+pecl channel-update pecl.php.net || true
+pecl install grpc || true
+pecl install protobuf || true
 protoc --proto_path=proto \
   --php_out=build/php \
-  --grpc_out=build/phpp \
+  --grpc_out=build/php \
   --plugin=protoc-gen-grpc=bins/opt/grpc_php_plugin \
   --experimental_allow_proto3_optional \
   proto/*.proto
